701c2db1f2d316e0956623c79208abef2f528cab
geu2 <geu2@tpu.ru>
2024-05-24 12:31:50 +0700

    Form padding corrected

--- ViewFigure2/MainForm.Designer.cs
+++ ViewFigure2/MainForm.Designer.cs
@@ -28,176 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.DeleteFigureButton = new System.Windows.Forms.Button();
-            this.AddFigureButton = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.CleanListButton = new System.Windows.Forms.Button();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
-            this.SaveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.OpenFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
-            this.FilterButton = new System.Windows.Forms.Button();
-            this.CleanFilterButton = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.toolStrip1.SuspendLayout();
-            this.SuspendLayout();
+            DeleteFigureButton = new Button();
+            AddFigureButton = new Button();
+            groupBox1 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            CleanListButton = new Button();
+            toolStrip1 = new ToolStrip();
+            toolStripDropDownButton1 = new ToolStripDropDownButton();
+            SaveFileToolStripMenuItem = new ToolStripMenuItem();
+            OpenFileToolStripMenuItem = new ToolStripMenuItem();
+            toolStripButton1 = new ToolStripButton();
+            FilterButton = new Button();
+            CleanFilterButton = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            toolStrip1.SuspendLayout();
+            SuspendLayout();
             // 
             // DeleteFigureButton
             // 
-            this.DeleteFigureButton.Location = new System.Drawing.Point(13, 255);
-            this.DeleteFigureButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.DeleteFigureButton.Name = "DeleteFigureButton";
-            this.DeleteFigureButton.Size = new System.Drawing.Size(192, 22);
-            this.DeleteFigureButton.TabIndex = 6;
-            this.DeleteFigureButton.Text = "Удалить фигуру";
-            this.DeleteFigureButton.UseVisualStyleBackColor = true;
-            this.DeleteFigureButton.Click += new System.EventHandler(this.DeleteFigureButton_Click);
+            DeleteFigureButton.Location = new Point(13, 255);
+            DeleteFigureButton.Margin = new Padding(3, 2, 3, 2);
+            DeleteFigureButton.Name = "DeleteFigureButton";
+            DeleteFigureButton.Size = new Size(192, 22);
+            DeleteFigureButton.TabIndex = 6;
+            DeleteFigureButton.Text = "Удалить фигуру";
+            DeleteFigureButton.UseVisualStyleBackColor = true;
+            DeleteFigureButton.Click += DeleteFigureButton_Click;
             // 
             // AddFigureButton
             // 
-            this.AddFigureButton.Location = new System.Drawing.Point(13, 225);
-            this.AddFigureButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.AddFigureButton.Name = "AddFigureButton";
-            this.AddFigureButton.Size = new System.Drawing.Size(192, 22);
-            this.AddFigureButton.TabIndex = 5;
-            this.AddFigureButton.Text = "Добавить фигуру";
-            this.AddFigureButton.UseVisualStyleBackColor = true;
-            this.AddFigureButton.Click += new System.EventHandler(this.AddFigureButton_Click);
+            AddFigureButton.Location = new Point(13, 225);
+            AddFigureButton.Margin = new Padding(3, 2, 3, 2);
+            AddFigureButton.Name = "AddFigureButton";
+            AddFigureButton.Size = new Size(231, 22);
+            AddFigureButton.TabIndex = 5;
+            AddFigureButton.Text = "Добавить фигуру";
+            AddFigureButton.UseVisualStyleBackColor = true;
+            AddFigureButton.Click += AddFigureButton_Click;
             // 
             // groupBox1
             // 
-            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.groupBox1.Controls.Add(this.dataGridView1);
-            this.groupBox1.Location = new System.Drawing.Point(4, 30);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBox1.Size = new System.Drawing.Size(443, 188);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Список фигур";
+            groupBox1.BackColor = SystemColors.ControlLightLight;
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Location = new Point(4, 30);
+            groupBox1.Margin = new Padding(3, 2, 3, 2);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(3, 2, 3, 2);
+            groupBox1.Size = new Size(463, 188);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Список фигур";
             // 
             // dataGridView1
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(9, 20);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(393, 164);
-            this.dataGridView1.TabIndex = 0;
+            dataGridView1.BackgroundColor = SystemColors.InactiveCaption;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(9, 20);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(448, 164);
+            dataGridView1.TabIndex = 0;
             // 
             // CleanListButton
             // 
-            this.CleanListButton.Location = new System.Drawing.Point(214, 225);
-            this.CleanListButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.CleanListButton.Name = "CleanListButton";
-            this.CleanListButton.Size = new System.Drawing.Size(192, 22);
-            this.CleanListButton.TabIndex = 7;
-            this.CleanListButton.Text = "Очистить список";
-            this.CleanListButton.UseVisualStyleBackColor = true;
-            this.CleanListButton.Click += new System.EventHandler(this.CleanListButton_Click);
+            CleanListButton.Location = new Point(250, 225);
+            CleanListButton.Margin = new Padding(3, 2, 3, 2);
+            CleanListButton.Name = "CleanListButton";
+            CleanListButton.Size = new Size(215, 22);
+            CleanListButton.TabIndex = 7;
+            CleanListButton.Text = "Очистить список";
+            CleanListButton.UseVisualStyleBackColor = true;
+            CleanListButton.Click += CleanListButton_Click;
             // 
             // toolStrip1
             // 
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripDropDownButton1,
-            this.toolStripButton1});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(414, 25);
-            this.toolStrip1.TabIndex = 8;
-            this.toolStrip1.Text = "toolStrip1";
+            toolStrip1.ImageScalingSize = new Size(20, 20);
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripDropDownButton1, toolStripButton1 });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(479, 25);
+            toolStrip1.TabIndex = 8;
+            toolStrip1.Text = "toolStrip1";
             // 
             // toolStripDropDownButton1
             // 
-            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.SaveFileToolStripMenuItem,
-            this.OpenFileToolStripMenuItem});
-            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
-            this.toolStripDropDownButton1.Size = new System.Drawing.Size(49, 22);
-            this.toolStripDropDownButton1.Text = "Файл";
+            toolStripDropDownButton1.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            toolStripDropDownButton1.DropDownItems.AddRange(new ToolStripItem[] { SaveFileToolStripMenuItem, OpenFileToolStripMenuItem });
+            toolStripDropDownButton1.ImageTransparentColor = Color.Magenta;
+            toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            toolStripDropDownButton1.Size = new Size(49, 22);
+            toolStripDropDownButton1.Text = "Файл";
             // 
             // SaveFileToolStripMenuItem
             // 
-            this.SaveFileToolStripMenuItem.Name = "SaveFileToolStripMenuItem";
-            this.SaveFileToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
-            this.SaveFileToolStripMenuItem.Text = "Сохранить файл";
-            this.SaveFileToolStripMenuItem.Click += new System.EventHandler(this.SaveFileToolStripMenuItem_Click);
+            SaveFileToolStripMenuItem.Name = "SaveFileToolStripMenuItem";
+            SaveFileToolStripMenuItem.Size = new Size(165, 22);
+            SaveFileToolStripMenuItem.Text = "Сохранить файл";
+            SaveFileToolStripMenuItem.Click += SaveFileToolStripMenuItem_Click;
             // 
             // OpenFileToolStripMenuItem
             // 
-            this.OpenFileToolStripMenuItem.Name = "OpenFileToolStripMenuItem";
-            this.OpenFileToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
-            this.OpenFileToolStripMenuItem.Text = "Открыть файл";
-            this.OpenFileToolStripMenuItem.Click += new System.EventHandler(this.OpenFileToolStripMenuItem_Click);
+            OpenFileToolStripMenuItem.Name = "OpenFileToolStripMenuItem";
+            OpenFileToolStripMenuItem.Size = new Size(165, 22);
+            OpenFileToolStripMenuItem.Text = "Открыть файл";
+            OpenFileToolStripMenuItem.Click += OpenFileToolStripMenuItem_Click;
             // 
             // toolStripButton1
             // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
-            this.toolStripButton1.Text = "toolStripButton1";
+            toolStripButton1.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButton1.ImageTransparentColor = Color.Magenta;
+            toolStripButton1.Name = "toolStripButton1";
+            toolStripButton1.Size = new Size(23, 22);
+            toolStripButton1.Text = "toolStripButton1";
             // 
             // FilterButton
             // 
-            this.FilterButton.Location = new System.Drawing.Point(214, 255);
-            this.FilterButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.FilterButton.Name = "FilterButton";
-            this.FilterButton.Size = new System.Drawing.Size(92, 22);
-            this.FilterButton.TabIndex = 9;
-            this.FilterButton.Text = "Фильтр";
-            this.FilterButton.UseVisualStyleBackColor = true;
-            this.FilterButton.Click += new System.EventHandler(this.FilterButton_Click);
+            FilterButton.Location = new Point(211, 255);
+            FilterButton.Margin = new Padding(3, 2, 3, 2);
+            FilterButton.Name = "FilterButton";
+            FilterButton.Size = new Size(101, 22);
+            FilterButton.TabIndex = 9;
+            FilterButton.Text = "Фильтр";
+            FilterButton.UseVisualStyleBackColor = true;
+            FilterButton.Click += FilterButton_Click;
             // 
             // CleanFilterButton
             // 
-            this.CleanFilterButton.Location = new System.Drawing.Point(315, 255);
-            this.CleanFilterButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.CleanFilterButton.Name = "CleanFilterButton";
-            this.CleanFilterButton.Size = new System.Drawing.Size(92, 22);
-            this.CleanFilterButton.TabIndex = 10;
-            this.CleanFilterButton.Text = "Сбросить";
-            this.CleanFilterButton.UseVisualStyleBackColor = true;
-            this.CleanFilterButton.Click += new System.EventHandler(this.CleanFilterButton_Click);
+            CleanFilterButton.Location = new Point(318, 255);
+            CleanFilterButton.Margin = new Padding(3, 2, 3, 2);
+            CleanFilterButton.Name = "CleanFilterButton";
+            CleanFilterButton.Size = new Size(147, 22);
+            CleanFilterButton.TabIndex = 10;
+            CleanFilterButton.Text = "Сбросить";
+            CleanFilterButton.UseVisualStyleBackColor = true;
+            CleanFilterButton.Click += CleanFilterButton_Click;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(414, 287);
-            this.Controls.Add(this.CleanFilterButton);
-            this.Controls.Add(this.CleanListButton);
-            this.Controls.Add(this.FilterButton);
-            this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.DeleteFigureButton);
-            this.Controls.Add(this.AddFigureButton);
-            this.Controls.Add(this.groupBox1);
-            this.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.MaximizeBox = false;
-            this.Name = "MainForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Расчёт площади фигур";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(479, 287);
+            Controls.Add(CleanFilterButton);
+            Controls.Add(CleanListButton);
+            Controls.Add(FilterButton);
+            Controls.Add(toolStrip1);
+            Controls.Add(DeleteFigureButton);
+            Controls.Add(AddFigureButton);
+            Controls.Add(groupBox1);
+            ForeColor = SystemColors.Desktop;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 2, 3, 2);
+            MaximizeBox = false;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Расчёт площади фигур";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
